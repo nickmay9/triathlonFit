e22983a131d80c0e976b7cf1e4954fc9cf701785
Nick May <nickmay9@outlook.com>
2021-05-08 13:06:22 -0400

    Initial Commit

---
+++ triathlonFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  triathlonFit
+//
+//  Created by Nicholas May on 5/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
